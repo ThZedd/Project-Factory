7d68f4787fec2ca40d7b0c0e127a05bc0e251090
ThZedd <ber.cai.car@gmail.com>
2026-03-08 04:11:49 +0000

    Testing some things

--- Washing_Hands/src/iot/main.cpp
+++ Washing_Hands/src/iot/main.cpp
@@ -1,94 +1,97 @@
 #include <Arduino.h>
-#include <WiFi.h>
-#include <Wire.h>
-
-// O LED azul integrado na placa costuma ser o pino 2
-#define LED_PIN 2
+#include <Adafruit_GFX.h>
+#include <Adafruit_ST7735.h>
+#include <SPI.h>
 
 
-const char *ssid = "ESP32-Access-Point";
-const char *password = "12345678";
+#define MAINBUTTON_PIN 13 // This button will make sure to advance to the next phase
 
-WiFiServer server(80);
+#define ANIMALBUTTON1_PIN 14 // This button will make sure to get if the children got the animal correct
+#define ANIMALBUTTON2_PIN 25 // This button will make sure to get if the children got the animal correct
+#define ANIMALBUTTON3_PIN 26 // This button will make sure to get if the children got the animal correct
+#define ANIMALBUTTON4_PIN 27 // This button will make sure to get if the children got the animal correct
+
+#define DFPLAYER_RX2_PIN 16
+#define DFPLAYER_TX2_PIN 17
+
+#define DISPLAY_SCK_PIN 18
+#define DISPLAY_SDA_PIN 23
+#define DISPLAY_RES_PIN 4
+#define DISPLAY_RS_DC_PIN 2
+#define DISPLAY_CS_PIN 5
+
+Adafruit_ST7735 display = Adafruit_ST7735(DISPLAY_CS_PIN, DISPLAY_RS_DC_PIN, DISPLAY_RES_PIN);
+// Variáveis para controlar o botão
+int lastButtonState = 1; // HIGH porque estamos a usar INPUT_PULLUP
+int currentPhase = 0;   // This will indicate in what phase are we in
+// Variáveis para controlar o Timer
 
 void setup()
 {
-  Serial.begin(115200);
+    Serial.begin(115200);
+    //Buttons Mode
+    pinMode(MAINBUTTON_PIN, INPUT_PULLUP);
+    pinMode(ANIMALBUTTON1_PIN, INPUT_PULLUP);
+    pinMode(ANIMALBUTTON2_PIN, INPUT_PULLUP);
+    pinMode(ANIMALBUTTON3_PIN, INPUT_PULLUP);
+    pinMode(ANIMALBUTTON4_PIN, INPUT_PULLUP);
 
-  delay(1000);
-  Serial.println("Setting AP...");
-  Serial.println("Teste de Toque Iniciado!");
+    //DFPlayer inicialized
+    //Serial2.begin(9600, SERIAL_8N1, DFPLAYER_RX2_PIN, DFPLAYER_TX2_PIN);
 
-  WiFi.softAP(ssid, password);
-  IPAddress IP = WiFi.softAPIP();
-  Serial.println(IP);
-
-  server.begin();
-
-  pinMode(LED_PIN, OUTPUT);
+    //Display inicialized
+    display.initR(INITR_BLACKTAB); 
+    display.setRotation(1); 
+    Serial.println("Sistema pronto!");
+    
 }
 
 void loop()
 {
-  WiFiClient client = server.available();
+    // Lê o estado atual do botão
+    int currentButtonState = digitalRead(MAINBUTTON_PIN);
 
-  if (client)
-  {
-    Serial.println("New Client.");
-    String currentLine = "";
-    while (client.connected())
+    // Verifica se o botão acabou de ser pressionado (mudou de HIGH para LOW)
+    if (lastButtonState == 1 && currentButtonState == 0)
     {
 
-      if (client.available())
-      {
-        char c = client.read();
-        Serial.write(c);
-        currentLine += c;
-        if (c == '\n')
+        switch (currentPhase)
         {
-          if (currentLine.length() == 2)
-          {
-            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-            // and a content-type so the client knows what's coming, then a blank line:
-            client.println("HTTP/1.1 200 OK");
-            client.println("Content-type:text/html");
-            client.println("Connection: close");
-            client.println();
-
-            // Send your "Hello World" HTML response
-            client.println("<html><head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"></head>");
-            client.println("<body><h1>Hello World</h1></body></html>");
-
-            // The HTTP response ends with another blank line
-            client.println();
-            // Break out of the while loop
+        //Não esquecer de colocar o case 0 no setup assim ao iniciar vai logo fazer oque o case 0 faz
+        case 0:
+            Serial.println("Olá bora lavar as mãos juntos??!!!");
+            Serial.println("Clica no botão denovo para começarmos!!!!");
             break;
-          }
-          else
-          { // if you got a newline, then clear currentLine
-            currentLine = "";
-          }
-        }
-        else if (c != '\r')
-        {                   // if you got anything else but a carriage return character,
-          currentLine += c; // add it to the end of the currentLine
-        }
-      }
-    }
-  }
-  // O pino D4 (GPIO 4) suporta toque capacitivo (Touch 0)
-  // Toca com o dedo no pino metálico D4 da tua placa
-  /*
-  int valorToque = touchRead(4);
+        case 1:
+            Serial.println("Antes de começarmos que tal tornarmos isto um pouco mais divertido?");
+            Serial.println("Vai aparecer um animal no ecrã e tu não te podes esquecer qual foi, ok?");
+            Serial.println("Clica no botão denovo para avançarmos!!!!");
+            break;
+        case 2:
+            //ira ser escolhido um animal qualquer ( entre 4 opcoes)
+            Serial.println("Clica no botão denovo quando tiveres o decorado!!!!");
+            break;
+        case 3:
+             Serial.println("Bora lá começar primeiro pomos o sabão na mão");
+             delay(3000); // 3 segundos de pausa, tempo medio que uma criança demora a colocar o sabão
+             Serial.println("E agora esfrega esfrega esfrega!!!");
+            break;
+        case 4:
+            /* code */
+            break;
+        case 5:
+            printf("Parabéns conseguiste!!!!");
+            break;
 
-  Serial.print("Valor do Sensor no Pino D4: ");
-  Serial.println(valorToque);
-  if (valorToque <= 60)
-  {
-    digitalWrite(LED_PIN, HIGH); // turn the LED on (HIGH is the voltage level)
-    delay(1000);                 // wait for a second
-    digitalWrite(LED_PIN, LOW);  // turn the LED off by making the voltage LOW
-    delay(1000);
-  }
-    */
+        default:
+            currentPhase = 0;
+            break;
+        }
+
+        delay(50);
+        currentPhase++;
+    }
+
+    // Atualiza o estado anterior do botão para a próxima volta do loop
+    lastButtonState = currentButtonState;
 }
